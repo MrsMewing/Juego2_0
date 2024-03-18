911c9afca540f9a69a1bbd51971745c35d882317
MrsMewing <163680936+MrsMewing@users.noreply.github.com>
2024-03-17 18:47:37 -0600

    Update Castle_Objects.py

--- Juego_Laberinto/Castle_Objects.py
+++ Juego_Laberinto/Castle_Objects.py
@@ -34,14 +34,32 @@ def crear_escenario(imagen, tamaño_escenario, coordenadas_escenario, tamaño_bl
 
     return cuadrados_escenario
 
-bloques_escenario = crear_escenario(ruta_bloque, [10, 10], [100, 200], [16, 16])
+def detectar_colision():
+    pass
 
-bloque = Bloque(ruta_bloque, [100, 100])
+
+bloques_escenario_2 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [330, 78], tamaño_bloques=[16, 16])
+bloques_escenario_3 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [80, 330], tamaño_bloques=[16, 16])
+bloques_escenario_4 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [15, 10], coordenadas_escenario= [330, 330], tamaño_bloques=[16, 16])
+#bloques_escenario_5 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [100, 200], tamaño_bloques=[16, 16])
+#bloques_escenario_6 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [100, 200], tamaño_bloques=[16, 16])
+
 
 class Habitacion(pygame.sprite.Sprite):
-    def __init__(self, tamaño, enemigos, trampas, tesoros = None):
+    def __init__(self, recursos_escenario, tamaño, coordenadas, enemigos, trampas, tesoros = None):
         super().__init__()
+        self.recursos_escenario = recursos_escenario
+        self.coordenadas = coordenadas
         self.tamaño = tamaño
         self.enemigos = enemigos
         self.trampas = trampas
         self.tesoros = tesoros
+    
+    def draw(self, ventana_juego):
+        escenario = crear_escenario(imagen = self.recursos_escenario, tamaño_escenario= self.tamaño, coordenadas_escenario= self.coordenadas, tamaño_bloques=[16, 16])
+
+        escenario.draw(ventana_juego)
+
+
+escenario_inicio = Habitacion(ruta_bloque, [10, 10], [80, 50], False, False, False)
+        
