6fb2d61aec78eedcbfe47a56e5907a380047b585
MrsMewing <163680936+MrsMewing@users.noreply.github.com>
2024-03-19 01:02:31 -0600

    Update Main.py

--- Juego_Laberinto/Main.py
+++ Juego_Laberinto/Main.py
@@ -1,15 +1,16 @@
+from colisiones import * 
 from Sprites import *
 from Castle_Objects import *
 import pygame
 
 pygame.init()
 
-ANCHO_VENTANA, ALTO_VENTANA = 900, 700
+ANCHO_VENTANA, ALTO_VENTANA = 1300, 700
 
 VENTANA_PRINCIPAL = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
 pygame.display.set_caption("Las aventuras de Juan")
 
-cartero = Personaje(nombre="Juan", objeto_spritesheets=sprites, coordenadas=(100, 250), velocidad=2, escenario_actual=escenario_inicio)
+cartero = Personaje(nombre="Juan", objeto_spritesheets=sprites, coordenadas=(100, 240), velocidad=2, escenario_actual=escenario_inicio)
 
 reloj = pygame.time.Clock()
 
@@ -22,11 +23,19 @@ while juego_terminado != True:
         if evento.type == pygame.QUIT:
             juego_terminado = True
 
-    VENTANA_PRINCIPAL.fill((255, 255, 255))
+    VENTANA_PRINCIPAL.fill((83, 76, 206))
 
     #bloque.draw(VENTANA_PRINCIPAL)
     #escenario_inicio.draw(VENTANA_PRINCIPAL)
     escenario_inicio.escenario.draw(VENTANA_PRINCIPAL)
+    escenario_de_reuiniones.escenario.draw(VENTANA_PRINCIPAL)
+    escenario_de_descanso.escenario.draw(VENTANA_PRINCIPAL)
+    escenario_principal.escenario.draw(VENTANA_PRINCIPAL)
+    escenario_de_tropas.escenario.draw(VENTANA_PRINCIPAL)
+
+    pasillos_mapa.draw(VENTANA_PRINCIPAL)
+
+    print(cartero.direccion_movimiento)
 
     #cartero.draw(VENTANA_PRINCIPAL)
     cartero.update(VENTANA_PRINCIPAL)
