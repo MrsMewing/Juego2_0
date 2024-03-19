918e813f29c77bde17dfd440707d8a568c8762ec
MrsMewing <163680936+MrsMewing@users.noreply.github.com>
2024-03-18 18:48:08 -0600

    Update Castle_Objects.py

--- Juego_Laberinto/Castle_Objects.py
+++ Juego_Laberinto/Castle_Objects.py
@@ -1,4 +1,6 @@
+from Sprites import *
 import pygame
+
 
 pygame.init()
 
@@ -44,22 +46,34 @@ bloques_escenario_4 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [
 #bloques_escenario_5 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [100, 200], tamaño_bloques=[16, 16])
 #bloques_escenario_6 = crear_escenario(imagen = ruta_bloque, tamaño_escenario= [10, 10], coordenadas_escenario= [100, 200], tamaño_bloques=[16, 16])
 
+class Escenario(pygame.sprite.Sprite):
+    def __init__(self, nombre_escenario, imagen, coordenadas, tamaño_escenario):
+        super().__init__()
+        self.nombre_escenario = nombre_escenario
+        self.imagen = pygame.transform.scale(imagen, (tamaño_escenario))
+        self.rect = self.imagen.get_rect()
+
+        self.rect.x = coordenadas[0]
+        self.rect.y = coordenadas[1]
+
+    def draw(self, ventana_juego):
+        ventana_juego.blit(self.imagen, self.rect)
+
 
 class Habitacion(pygame.sprite.Sprite):
-    def __init__(self, recursos_escenario, tamaño, coordenadas, enemigos, trampas, tesoros = None):
+    def __init__(self, nombre_habitacion, tamaño_habitacion, coordenadas, enemigos, trampas, tesoros = None):
         super().__init__()
-        self.recursos_escenario = recursos_escenario
         self.coordenadas = coordenadas
-        self.tamaño = tamaño
+        self.escenario = Escenario(nombre_habitacion, recortar_imagen(pygame.image.load("assets_villa/dungeon_tiles.png"), 10, 10, 105, 115), self.coordenadas, tamaño_habitacion)
         self.enemigos = enemigos
         self.trampas = trampas
         self.tesoros = tesoros
     
     def draw(self, ventana_juego):
-        escenario = crear_escenario(imagen = self.recursos_escenario, tamaño_escenario= self.tamaño, coordenadas_escenario= self.coordenadas, tamaño_bloques=[16, 16])
+        escenario = crear_escenario(imagen = self.escenario, tamaño_escenario= self.tamaño, coordenadas_escenario= self.coordenadas, tamaño_bloques=[16, 16])
 
         escenario.draw(ventana_juego)
 
 
-escenario_inicio = Habitacion(ruta_bloque, [10, 10], [80, 50], False, False, False)
-        
+escenario_inicio = Habitacion("colonia", (250, 250), [80, 50], False, False, False)
+#escenario_de_reuiniones = Habitacion(ruta_bloque, [10, 10], [80, 330], False, False, False)
